54c504f4512a68c1ea02e91f21bd80e00f374027
sandervw <14879109+sandervw@users.noreply.github.com>
2026-04-01 08:48:40 -0500

    added cloudflare inage adapter with build-time compile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 
 export default defineConfig({
   site: 'https://www.cheddarsoap.com',
   output: 'static',
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
 });
